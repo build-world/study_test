1d190cf60fa9526cc16863886ecc275589fd5194
limithink <xqq1024@live.com>
2017-06-12 19:05:02 +0800

    change cn-zh comment to en

--- Matlab/pcsx_2.m
+++ Matlab/pcsx_2.m
@@ -1,7 +1,7 @@
 %Copyright 2017 Fan Yifeng. All rights reserved.
 
-%?????????
-%rho def
+%global adjust measurement use degree and meter
+%rho def 1rad = 180/pi degree
 rho = 180/pi;
 %original data
 r = 3;
@@ -12,7 +12,7 @@ y_C = -796.549;
 alpha_AB = dms2degrees([226 44 59]);
 alpha_CD = dms2degrees([324 46 03]);
 L = [204.952;200.130;345.153;dms2degrees([230 32 37]);dms2degrees([180 00 42]);dms2degrees([170 39 22]);dms2degrees([236 48 37])];
-%????
+%approximate values calc
 Alpha = [alpha_AB - 180 + L(4,1);alpha_AB - 360 + L(4,1) + L(5,1);alpha_AB - 540 + L(4,1) + L(5,1) + L(6,1);alpha_AB - 720 + L(4,1) + L(5,1) + L(6,1) + L(7,1)];
 COS = zeros(3,1);
 SIN = zeros(3,1);
@@ -38,7 +38,7 @@ for i = 1:4
     A(2,i + 3) = -1/rho*(Y(4,1) - Y(i,1));
     A(3,i + 3) = 1/rho*(X(4,1) - X(i,1));
 end
-%W
+%W calc
 W = [Alpha(4,1) - alpha_CD;X(4,1) - x_C;Y(4,1) - y_C];
 %power matrix def
 P = zeros(7);
@@ -54,7 +54,7 @@ NAA = A*Q*A';
 K = -inv(NAA)*W;
 V = Q*A'*K;
 Lad = L + V;
-%
+%adjusted values calc
 Alphaad = [alpha_AB - 180 + Lad(4,1);alpha_AB - 360 + Lad(4,1) + Lad(5,1);alpha_AB - 540 + Lad(4,1) + Lad(5,1) + Lad(6,1);alpha_AB - 720 + Lad(4,1) + Lad(5,1) + Lad(6,1) + Lad(7,1)];
 COSad = zeros(3,1);
 SINad = zeros(3,1);
