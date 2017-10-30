c9aa5724f9ee2265a174c875c2eff4b11f77fbb5
limithink <xqq1024@live.com>
2017-10-30 23:01:00 +0800

    clean

--- Python/ArraySortCollection/main.py
+++ Python/ArraySortCollection/main.py
@@ -1,10 +1,10 @@
 #
 import SelectSort
-import tool 
+from tool import *
 array = [7, 9, 6, 1, 5, 3, 2, 4, 8, 0]
 print "source array", array, "length=", len(array)
 SelectSort.SelectSort_imp(array)
-ret = tool.OrderCheck(array,"asc")
+ret = OrderCheck(array,"asc")
 print "Sorted array", array, "length=", len(array)
 if ret == True: print "Order True"
 elif ret == False: print "Order False"
