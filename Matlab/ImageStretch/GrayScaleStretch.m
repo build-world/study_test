ac8a4b690b4df107ddd4ca7c69d81ed624f8232f
limithink <limithink@live.com>
2018-04-06 22:46:07 +0800

    exp() stretch

--- Matlab/ImageStretch/GrayScaleStretch.m
+++ Matlab/ImageStretch/GrayScaleStretch.m
@@ -1,10 +1,18 @@
 rc = 1024;
 for ctr = 1:rc
     if ctr <= rc/3
+        dx = 2 / (rc - 1);
+        min = exp(-1);
+        max = exp(1);
+        span = max - min;
+        for cur = 1:rc
+            img(ctr, cur) = (exp(-1 + dx * cur) - min)/span;
+        end
+    elseif ctr > rc/3 && ctr <= rc/3*2
         for cur = 1:rc
             img(ctr, cur) = (cur - 1)/rc;
         end
-    elseif ctr >rc/3 && ctr <= rc/3*2
+    else
         dx = 2 / rc;
         min = log(dx);
         max = log(2);
@@ -12,15 +20,7 @@ for ctr = 1:rc
         for cur = 1:rc
             img(ctr, cur) = (log(dx * cur) - min)/span;
         end
-    else
-        dx = 2 / rc;
-        min = log10(dx);
-        max = log10(2);
-        span = max - min;
-        for cur = 1:rc
-            img(ctr, cur) = (log10(dx * cur) - min)/span;
-        end
     end
 end
 imshow(img);
-imwrite(im2uint8(img), '.\A1.tif', 'tiff');
+imwrite(im2uint8(img), '.\A2.tif', 'tiff');
