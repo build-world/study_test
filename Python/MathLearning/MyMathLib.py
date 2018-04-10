eb52dc1040c1293a92a38d7bb23aac8b5c2efbd5
limithink <limithink@live.com>
2018-04-10 22:35:58 +0800

    add exp function

--- Python/MathLearning/MyMathLib.py
+++ Python/MathLearning/MyMathLib.py
@@ -1,10 +1,13 @@
-def myabs(num):
+#MyMathLib for studying math
+
+#Foundation
+def abs(num):
     if num >= 0:
         return num
     else:
         return -num
 
-def myfact(num):
+def fact(num):
     mul = 1
     ctr = num
     while ctr > 0:
@@ -12,18 +15,44 @@ def myfact(num):
         ctr -= 1
     return mul
 
-def mysin(rad):
+#By Taylor formula
+#Thanks to Taylor, I know the significance of constant e.
+def exp(x, precision):
     ctr = 0
     sum = 0.0
     prev = 1.0
-    while myabs(sum - prev) >= 0.0000001:
+    while abs(sum - prev) >= precision:
+        prev = sum
+        sum += 1.0 / fact(ctr) * (x ** ctr)
+        ctr += 1
+    return sum
+
+def sin(rad, precision):
+    ctr = 0
+    sum = 0.0
+    prev = 1.0
+    while abs(sum - prev) >= precision:
         if ctr % 4 != 0 and ctr % 4 != 2:
             if ctr % 4 == 1:
                 sign = 1
             else:
                 sign = -1
             prev = sum
-            sum += sign / myfact(ctr) * (rad ** ctr)
+            sum += sign / fact(ctr) * (rad ** ctr)
         ctr += 1
     return sum
 
+def cos(rad, precision):
+    ctr = 0
+    sum = 0.0
+    prev = 1.0
+    while abs(sum - prev) >= precision:
+        if ctr % 4 != 1 and ctr % 4 != 3:
+            if ctr % 4 == 0:
+                sign = 1
+            else:
+                sign = -1
+            prev = sum
+            sum += sign / fact(ctr) * (rad ** ctr)
+        ctr += 1
+    return sum
--- Python/MathLearning/main.py
+++ Python/MathLearning/main.py
@@ -1,4 +1,4 @@
 #-*- coding: UTF-8 -*-
-import MyMathLib
-x = float(input())
-print(MyMathLib.mysin(x))
+import MyMathLib as mml
+x = float(input("x = "))
+print("exp = ", mml.exp(x, 1e-16), "\nsin x = ", mml.sin(x, 1e-16), "\ncos x = ", mml.cos(x, 1e-16))
